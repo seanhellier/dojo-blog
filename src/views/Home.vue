793a7e10b58fc98c5579ee8b86eb70a19464651a
Sean Leif Hellier <sean@newtropolis.com>
2021-07-18 15:21:24 -0400

    lesson 75

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,32 @@
     <input type="text" v-model="search">
     <p>search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, reactive, ref } from 'vue'
+import { computed, reactive, ref, watch, watchEffect } from 'vue'
 export default {
   name: 'Home',
   setup() {    
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
     const matchingNames = computed(() => {
-      // return ['a', 'b', 'c']
       return names.value.filter(name => name.includes(search.value))
     })
-    return { names, search, matchingNames }
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect ran', search.value)
+      console.log(names.value)
+    })
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+    return { names, search, matchingNames, handleClick }
   },
 }
 </script>
